51449f994d0903822a691af22dc0abef1f96eb68
Kanchan Chauhan <kanchan827@gmail.com>
2021-05-20 21:07:10 -0700

    add hashrouter

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
+import Search from './Search';
 
 const App = () => (
-  <div>
-    <h1>My Shows</h1>
-    <input type="text" placeholder="Search" />
-    <a>or Browse All</a>
-  </div>
+  <HashRouter>
+    <div>
+      <Route exact path="/" component={Landing} />
+      <Route exact path="/search" component={Search} />
+    </div>
+  </HashRouter>
 );
 
 export default App;
---
+++ client/components/Search.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Search = () => (
+  <div>
+    <h1>Basic Search</h1>
+  </div>
+);
+
+export default Search;
